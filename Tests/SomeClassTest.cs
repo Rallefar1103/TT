6bbfcbb7f60f5d05054ec1ee430e3b64ff6daaed
MartinBruun <mbm93fen@gmail.com>
2020-09-06 09:26:48 +0200

    Apply proper assertion methods in SomeClassTest

--- Tests/SomeClassTest.cs
+++ Tests/SomeClassTest.cs
@@ -26,8 +26,8 @@ namespace Tests
 
             int ActualValue = StubObject.AddOne(ArrangedValue);
 
-            Assert.IsTrue(
-                ExpectedValue == ActualValue,
+            Assert.AreEqual(
+                ExpectedValue, ActualValue,
                 TestName
             );
         }
@@ -42,20 +42,21 @@ namespace Tests
 
             MockObject.IncreaseSalary(ArrangedValue);
 
-            Assert.IsTrue(
-                ExpectedValue == MockObject.Salary, 
+            Assert.AreEqual(
+                ExpectedValue, MockObject.Salary, 
                 TestName 
             );
         }
-        [Test]
-        public void IncreaseSalary_NegativeNumber_ShouldThrowException()
+        [TestCase(-3, "Salary must not increase by one", "Testing for message in SomeClass.IncreaseSalary when a negative number is given")]
+        public void IncreaseSalary_NegativeNumber_ShouldThrowException(int Arranged, string Expected, string TestName)
         {
-            int ArrangedValue = -3;
+            int ArrangedValue = Arranged;
+            string ExpectedString = Expected;
             int INITIAL_SALARY = 0;
             SomeClass MockObject = FakeSomeClass(INITIAL_SALARY);
 
             CustomException exception = Assert.Throws<Project.CustomException>(() => MockObject.IncreaseSalary(ArrangedValue));
-            Assert.AreEqual(exception.Message, "Salary must not increase by one", "Testing for message in SomeClass.IncreaseSalary when a negative number is given");
+            Assert.AreEqual(exception.Message, ExpectedString, TestName);
         }
     }
 }
