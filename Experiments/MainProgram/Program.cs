2ec4b98fa8c8a3d07a92605822142f81d8e6f752
MartinBruun <mbm93fen@gmail.com>
2020-09-18 15:26:09 +0200

    Add Experiments Folder for our experiments in programming

---
+++ Experiments/MainProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.WriteTextStaticParent();
+            Class2.WriteTextStaticChild();
+        }
+    }
+}
