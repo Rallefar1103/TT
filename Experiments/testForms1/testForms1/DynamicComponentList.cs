1177aa57ac319ef567aa4ee22c881969141872c2
Jakob Skov Søndergård <saxjax@saxjax.dk>
2020-09-29 13:40:07 +0200

    added first mobile layout for TurfTank

---
+++ Experiments/testForms1/testForms1/DynamicComponentList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace testForms1
+{
+    public class DynamicComponentList: INotifyPropertyChanged
+    {
+        public DynamicComponentList()
+        {
+        }
+        private ObservableCollection<Component> _componentList;
+        private Component _mySelectedComponent;
+
+
+        public ObservableCollection<Component> ComponentList
+        {
+            get { return _componentList; }
+            set {
+                    _componentList = value;
+                    OnPropertyChanged();
+                }
+        }
+
+        public Component MySelectedComponent
+        {
+            get { return _mySelectedComponent; }
+            set
+                {
+                    MySelectedComponent = value;
+                    OnPropertyChanged();
+                }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
---
+++ Experiments/testForms1/testForms1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing;
+
+namespace testForms1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BarcodeScanView.IsVisible = true;
+            BarcodeScanView.IsScanning = true;
+            BindingContext = new DynamicComponentList();
+            Title = "Component List";
+            
+        }
+
+        public void Generate_Barcode(object sender, EventArgs e)
+        {
+            BarcodeImageView.BarcodeValue = $"HEY TURFTANK {e}";
+            BarcodeImageView.IsVisible = true;
+        }
+
+        public void Handle_OnScanResult(Result result)
+        {
+            if (string.IsNullOrWhiteSpace(result.Text)) { return; }
+
+            //BarcodeImageView.IsVisible = !BarcodeImageView.IsVisible;
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Serial.Text = result.Text;
+            });
+            BarcodeScanView.IsScanning = true;
+        }
+        
+    }
+}
