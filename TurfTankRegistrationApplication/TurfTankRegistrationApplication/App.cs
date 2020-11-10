a374c295f48e6734c38bf7f091ce8c5bb5b2edfe
Jakob Skov Søndergård <saxjax@Jakobs-MacBook-Pro.local>
2020-11-11 00:43:57 +0100

    Jeg har lavet pages med Forms.Markup som bruger C# fremfor XAML, der er ingen navigation, det gøres ved at ændre mainpage i App.cs

---
+++ TurfTankRegistrationApplication/TurfTankRegistrationApplication/App.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Markup;
+using TurfTankRegistrationApplication.Pages;
+
+namespace TurfTankRegistrationApplication
+{
+	public class App : Application
+	{
+		public App()
+		{
+			Device.SetFlags(new string[] { "Markup_Experimental" });
+			MainPage = new TurfTankRegistrationApplication.Pages.RobotRegistrationPage();
+		}
+
+		protected override void OnStart()
+		{
+			System.Console.WriteLine("\n*************\nProgram will start up now enjoy the ride!\n****************\n");
+		}
+
+		protected override void OnSleep()
+		{
+			System.Console.WriteLine("\n*************\nYour tablet is sooooo tired and will go take a morfar nap!\n****************\n");
+		}
+
+		protected override void OnResume()
+		{
+			System.Console.WriteLine("\n*************\nHey there, Im Freshhhh!\n****************\n");
+		}
+
+	}
+}
+
