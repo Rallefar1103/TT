442e323ff9fe2dda5edce69feab26243a0529859
Rallefar1103 <rhenri18@student.aau.dk>
2020-11-22 09:48:21 +0100

    created bluetooth class

---
+++ TurfTankRegistrationApplication/TurfTankRegistrationApplication.Android/BluetoothDeviceReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.Bluetooth;
+using Android.Content;
+
+namespace TurfTankRegistrationApplication.Droid
+{
+    public class BluetoothDeviceReceiver : BroadcastReceiver
+    {
+        public BluetoothDeviceReceiver()
+        {
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            string action = intent.Action;
+
+            if (action == BluetoothDevice.ActionFound)
+            {
+                BluetoothDevice newDevice = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
+            }
+        }
+    }
+}
