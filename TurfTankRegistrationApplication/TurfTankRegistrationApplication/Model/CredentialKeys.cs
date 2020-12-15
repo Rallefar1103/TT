128d6f59e4c515ee74d411414158b39da7738696
MartinBruun <mbm93fen@gmail.com>
2020-12-15 22:31:00 +0100

    Update gitignore

--- TurfTankRegistrationApplication/TurfTankRegistrationApplication/Model/CredentialKeys.cs
+++ TurfTankRegistrationApplication/TurfTankRegistrationApplication/Model/CredentialKeys.cs
@@ -6,11 +6,11 @@ namespace TurfTankRegistrationApplication.Model
 {
     public partial class CredentialKeys
     {
-        public static string TestClientId = "n";
-        public static string TestClientSecret = "n";
-        public static string DevelopmentClientId = "n";
-        public static string DevelopmentClientSecret = "n";
-        public static string ProductionClientId = "n";
-        public static string ProductionClientSecret = "n";
+        public static string TestClientId = "X5goHHBzVSkwc8kwWpbw5fq9NzoVkAof";
+        public static string TestClientSecret = "KAaqom27hUCbyhUr1KtAO1o-ooxLJUOHn2o6YPmX0OopbD4FjgZnzVzhHynF8SM8";
+        public static string DevelopmentClientId = "aDIZm4O7cMp2jnUZIIR8VBaE9CyUWDrK";
+        public static string DevelopmentClientSecret = "zk_S6lwEPEUuEdChwH_NhD6w1dtWfsIi5qS2RgrKTIeYuFAQiZE2N_2qkZQLsGbB";
+        public static string ProductionClientId = "OT3kqkpjDvmSZl7WMjvwo4U72k1MWVUw";
+        public static string ProductionClientSecret = "KAaqom27hUCbyhUr1KtAO1o-ooxLJUOHn2o6YPmX0OopbD4FjgZnzVzhHynF8SM8";
     }
 }
