a7117734e2d01331843679ddcf9a9be4cfffce21
Rallefar1103 <rhenri18@student.aau.dk>
2020-12-17 15:31:24 +0100

    adds the base prereg vew model

---
+++ TurfTankRegistrationApplication/TurfTankRegistrationApplication/ViewModel/BasePreregistrationViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using TurfTankRegistrationApplication.Model;
+using TurfTankRegistrationApplication.Pages;
+using Xamarin.Forms;
+
+namespace TurfTankRegistrationApplication.ViewModel
+{
+    public class BasePreregistrationViewModel : INotifyPropertyChanged
+    {
+        public INavigation Navigation { get; set; }
+
+        public string QRScannedData { get; set; }
+
+        public QRSticker BaseQR { get; set; }
+        public SimCard BaseSimcard { get; set; }
+
+        public Action<object, string> Callback { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command ScanBaseQR { get; }
+        public Command ScanBaseSim { get; }
+        public Command ConfirmPreregistration { get; }
+
+        // UI Bools
+        public bool CanScanSQ { get; set; } = true;
+        public bool CanScanBarcode { get; set; } = false;
+        public bool CanConfirm { get; set; } = false;
+
+        // UI Color properties
+        public Color ScanQRColor { get; set; }
+        public Color ScanBarcodeColor { get; set; }
+
+        public BasePreregistrationViewModel(INavigation nav)
+        {
+            this.Navigation = nav;
+            this.BaseQR = new QRSticker();
+            CanScanSQ = true;
+            Callback = new Action<object, string>(OnDataReceived);
+            ScanBaseQR = new Command(execute: async () => await DummyScanQR(), canExecute: () => CanScanSQ);
+            ScanBaseSim = new Command(execute: async () => await DummyScanBarcode(), canExecute: () => CanScanBarcode);
+            ConfirmPreregistration = new Command(execute: async () => await DummyConfirm());
+            MessagingCenter.Subscribe<ScanPage, string>(this, "Result", Callback);
+        }
+
+
+        private async Task DummyScanQR()
+        {
+            await Task.Delay(2000);
+            await Application.Current.MainPage.DisplayAlert("Success!", "Scanned the Rover QR-label ", "OK");
+
+            CanScanBarcode = true;
+            OnPropertyChanged(nameof(CanScanBarcode));
+
+            ScanQRColor = Color.DarkGreen;
+            OnPropertyChanged(nameof(ScanQRColor));
+
+        }
+
+        private async Task DummyScanBarcode()
+        {
+            await Task.Delay(2000);
+            await Application.Current.MainPage.DisplayAlert("Success!", "Scanned the barcode", "Ok");
+
+            CanConfirm = true;
+            OnPropertyChanged(nameof(CanConfirm));
+
+            ScanBarcodeColor = Color.DarkGreen;
+            OnPropertyChanged(nameof(ScanBarcodeColor));
+
+        }
+
+        private async Task DummyConfirm()
+        {
+            await Task.Delay(2000);
+            await Application.Current.MainPage.DisplayAlert("Success!", "You succesfully preregistered the Rover", "Ok");
+            await Navigation.PopAsync();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void NavigateToScanPage(string component)
+        {
+            ScanPage scanPage = new ScanPage();
+            scanPage.vm.Title = "Scanning " + component;
+            
+            Navigation.PushAsync(scanPage);
+        }
+
+        private async void OnDataReceived(object sender, string data)
+        {
+            try
+            {
+                if (data.Contains("basestation"))
+                {
+                    QRScannedData = data;
+                    BaseQR = new QRSticker(data);
+                    await Application.Current.MainPage.DisplayAlert("Success!", "Scanned the Base QR-label " + BaseQR.ID, "OK");
+
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("OBS!", "Scan did not work as expected!", "Ok");
+                }
+            }
+            catch (Exception e)
+            {
+                await Application.Current.MainPage.DisplayAlert("OBS!", e.Message, "Ok");
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
